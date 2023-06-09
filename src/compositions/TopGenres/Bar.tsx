85adf1e518eab098af90bab2fe3c42a38d7c5648
Jaden Bertino <jaden@bertinofamily.com>
2023-06-09 11:41:57 -0700

    create color bar animations for TopGenre

--- src/compositions/TopGenres/Bar.tsx
+++ src/compositions/TopGenres/Bar.tsx
@@ -1,19 +1,45 @@
 import { CSSProperties } from 'react';
+import { interpolate, spring, useCurrentFrame, useVideoConfig } from 'remotion';
+
+const BAR_HEIGHT = 240
+const TOTAL_RANKS = 5
 
 interface BarProps {
   rank: number;
   genre: string;
   style?: CSSProperties;
+  endWidth: number;
 }
 
-const BAR_HEIGHT = 240
+export const Bar = ({ style, rank, genre, endWidth }: BarProps) => {
+  const { fps, width } = useVideoConfig();
+  const frame = useCurrentFrame();
 
-export const Bar = ({ style, rank, genre }: BarProps) => {
+  const opacityDelay = (TOTAL_RANKS - rank) * 3 + 10
+  const opacity = interpolate(frame - opacityDelay, [0, 8], [0, 1]);
+
+  const barSpring = spring({
+    frame: frame - (rank * 3) - 30,
+    fps,
+    config: {
+      damping: 200
+    }
+  })
+  const leftStart = (width - BAR_HEIGHT) / 2;
+  const leftOffset = interpolate(
+    barSpring, 
+    [0, 1],
+    [leftStart, -BAR_HEIGHT / 2],
+  )
+
+  const barWidth = interpolate(barSpring, [0, 1], [BAR_HEIGHT, endWidth])
+
   return (
     <div className='bar-wrapper' style={{
       display: 'flex',
       alignItems: 'center',
       color: '#fff',
+      width: width * 2,
     }}>
       <div style={{
         height: `${BAR_HEIGHT}px`,
@@ -21,7 +47,9 @@ export const Bar = ({ style, rank, genre }: BarProps) => {
         backgroundColor: '#fff',
         marginTop: '16px',
         boxShadow: '0 0 4px rgba(0, 0, 0, 0.3)',
-        width: '100%',
+        width: `${barWidth}px`,
+        opacity: opacity,
+        marginLeft: leftOffset,
         ...style,
       }}></div>
 
@@ -31,6 +59,7 @@ export const Bar = ({ style, rank, genre }: BarProps) => {
         fontSize: '40px',
         lineHeight: 1.5,
         textShadow: '0 0 4px rgba(0, 0, 0, 0.3)',
+        opacity: 0,
       }}>
         #{rank}<br />{genre}
       </p>
--- src/compositions/TopGenres/index.tsx
+++ src/compositions/TopGenres/index.tsx
@@ -1,8 +1,10 @@
-import { AbsoluteFill } from 'remotion';
+import { AbsoluteFill, useVideoConfig } from 'remotion';
 import { Title } from '../../components/Title';
 import { Bar } from './Bar';
 
 export const TopGenres = () => {
+  const { width } = useVideoConfig();
+
   return (
     <AbsoluteFill style={{
       backgroundColor: "#d4158e", 
@@ -11,32 +13,32 @@ export const TopGenres = () => {
       <div className='bars' style={{ marginTop: 440}}>
         <Bar style={{
           backgroundColor: '#b2fef6',
-          maxWidth: '60%',
         }} 
+        endWidth={width * 0.60}
         rank={1}
         genre={"Rap"} />
         <Bar style={{
           backgroundColor: '#d5f872',
-          maxWidth: '54%',
         }} 
+        endWidth={width * 0.54}
         rank={2}
         genre={"Pop"} />
         <Bar style={{
           backgroundColor: '#4e01f8',
-          maxWidth: '48%',
         }}
+        endWidth={width * 0.48}
         rank={3}
         genre={"Underground Hip Hop"} />
         <Bar style={{
           backgroundColor: '#1d1416',
-          maxWidth: '42%',
         }}
+        endWidth={width * 0.42}
         rank={4}
         genre={"Alternative R&B"} />
         <Bar style={{
           backgroundColor: '#fefefe',
-          maxWidth: '36%',
         }}
+        endWidth={width * 0.36}
         rank={5}
         genre={"Neo Soul"} />
       </div>
